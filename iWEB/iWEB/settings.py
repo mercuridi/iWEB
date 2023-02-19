9e11799edd4c489bb729a54da0a28e7d6bbe4228
Mercuridi <kab237@exeter.ac.uk>
2023-02-19 15:45:28 +0000

    update settings to switch to SQL

--- iWEB/iWEB/settings.py
+++ iWEB/iWEB/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'iWEB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'IWEB',
+        'USER': 'iweb_dev',
+        'PASSWORD': 'grp30',
+        'HOST': 'localhost',
     }
 }
 
