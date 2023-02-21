9eb661447bf3d046ad485cebf95e5ef7fa1cb947
Beth1612 <95871846+Beth1612@users.noreply.github.com>
2023-02-21 12:57:30 +0000

    Add files via upload

--- iWEB/app/urls.py
+++ iWEB/app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('addLocation', views.addLocation, name='add-location'),
+    path('register', views.register_request, name='register'),
+    path("login", views.login_request, name="login")
 ]
