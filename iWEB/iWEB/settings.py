19856cebfe556908ccfafe8f73b08c46d5826879
DimitarSivrev <96288224+DimitarSivrev@users.noreply.github.com>
2023-02-21 03:46:08 +0000

    Add files via upload
    Switched from SQLlite to PostgreSQL.

--- iWEB/iWEB/settings.py
+++ iWEB/iWEB/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'iWEB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iwebdatabase',
+        'USER': 'admin_db',
+        'PASSWORD': 'iWEBB',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
