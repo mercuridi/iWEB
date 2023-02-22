7d19100ad13285dc2ba2ca8e5164bc6c8c04c284
CheeseAndHamBagel <judegoulding@hotmail.com>
2023-02-22 17:11:46 +0000

    Started working with bootstrap - not quite working test.html and offcanva.* are a template

--- iWEB/app/urls.py
+++ iWEB/app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('',views.index,name='index'),
+    path('test', views.test,name='test'),
+    path('home', views.home, name='home'),
     path('addLocation', views.addLocation, name='add-location'),
 ]
--- iWEB/app/views.py
+++ iWEB/app/views.py
@@ -6,6 +6,16 @@ from .models import Location, Item
 from .forms import LocationForm
 # Create your views here.
 
+def index(request):
+    all_items = Item.objects.all
+    all_locations = Location.objects.all
+    return render(request, 'index.html', {'all_items': all_items, 'all_locations': all_locations})
+
+def test(request):
+    all_items = Item.objects.all
+    all_locations = Location.objects.all
+    return render(request, 'test.html', {'all_items': all_items, 'all_locations': all_locations})
+
 def home(request):
     all_items = Item.objects.all
     all_locations = Location.objects.all
