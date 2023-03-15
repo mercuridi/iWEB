deb5b520dc4ef106729fa767b31eea5b5cc3145b
Mercuridi <kab237@exeter.ac.uk>
2023-03-15 12:33:41 +0000

    linting

--- app/iWEB/tests/test_login.py
+++ app/iWEB/tests/test_login.py
@@ -7,16 +7,17 @@ from django.contrib.auth.models import User
 # Imported for you ^^ https://docs.djangoproject.com/en/4.1/topics/testing/tools/
 c = Client()
 
+
 class LoginClass(TestCase):
-    #@classmethod
-    #def setUp(self):
+    # @classmethod
+    # def setUp(self):
     #    self.credentials = {
     #        'username': 'testuser',
     #        'password': 'secret'}
     #    User.objects.create_user(**self.credentials)
-    #def test_login(self):
+    # def test_login(self):
     #    # login
-    #    response = self.client.post('/login/', self.credentials, )      
+    #    response = self.client.post('/login/', self.credentials, )
     #    # should be logged in now
     #    self.assertTrue(response.context['user'].is_authenticated)
     pass
--- app/iWEB/tests/test_utils.py
+++ app/iWEB/tests/test_utils.py
@@ -4,6 +4,7 @@ from app.models import Challenge
 from app.models import UserProfile
 from app.models import User
 
+
 def populate_test_database():
     """
     Script to consistently populate the database with standard testing data
@@ -11,36 +12,37 @@ def populate_test_database():
     """
     # Locations
     # 1 location manually setting everything except type (should autofill Fountain)
-    Location.objects.create(building    = "Building Test (Fountain)",
-                            longitude   = 1.22,
-                            latitude    = 1.55555,
-                            information = "Test information field",
-                            usable      = False)
+    Location.objects.create(building="Building Test (Fountain)",
+                            longitude=1.22,
+                            latitude=1.55555,
+                            information="Test information field",
+                            usable=False)
     # 1 location only setting the type
-    Location.objects.create(type = "BusStop")
+    Location.objects.create(type="BusStop")
 
     # Items
     # 1 item manually setting everything
-    Item.objects.create(name = "Test item 1", 
-                        description = "Item test description, cost 1000",
-                        price = 1000)
+    Item.objects.create(name="Test item 1",
+                        description="Item test description, cost 1000",
+                        price=1000)
     # 1 item only setting the name
-    Item.objects.create(name = "Test item 2")
-    
+    Item.objects.create(name="Test item 2")
+
     # Challenges
     # 1 challenge manually setting everything
-    Challenge.objects.create(name = "Test challenge 1",
-                                description = "Challenge test description, difficulty 3",
-                                difficulty = 3)
+    Challenge.objects.create(name="Test challenge 1",
+                             description="Challenge test description, difficulty 3",
+                             difficulty=3)
     # 1 challenge only setting the name
-    Challenge.objects.create(name = "Test challenge 2")
-    
+    Challenge.objects.create(name="Test challenge 2")
+
     # create some users :)
     next_username = "Test User {num}"
-    for i in range(10): # adjust range for more or less users, 10 is enough
-                        # don't set to less than 5 or some tests fail (intended behaviour)
+    for i in range(10):  # adjust range for more or less users, 10 is enough
+        # don't set to less than 5 or some tests fail (intended behaviour)
         # create a user and its profile
-        new_user = User.objects.create(username = next_username.format(num = i), password = "PASSWORDTEST")
+        new_user = User.objects.create(
+            username=next_username.format(num=i), password="PASSWORDTEST")
         profile = new_user.profile
         # populate with "typical" values
         profile.streak = i
