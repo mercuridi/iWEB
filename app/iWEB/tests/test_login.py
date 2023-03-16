cab2d7f7a165a005d038912361b5ad5e901ed17d
Dash <ds702@exeter.ac.uk>
2023-03-16 20:33:54 +0000

    tests for user register

--- app/iWEB/tests/test_login.py
+++ app/iWEB/tests/test_login.py
@@ -12,7 +12,12 @@ c = Client()
 class RegisterClass(TestCase):
     def setUp(self):
         self.register_url=reverse('register')
-
+        self.user={
+            'username':'username', 
+            'email':'email@email.com', 
+            'password1':'password', 
+            'password2':'password'
+        }
         return super().setUp()
 
 class RegisterTest(RegisterClass):
@@ -20,3 +25,7 @@ class RegisterTest(RegisterClass):
         response=self.client.get(self.register_url)
         self.assertEqual(response.status_code, 200) #page is successfully accessed
         self.assertTemplateUsed(response, 'register.html') #template is successfully used
+
+    def test_can_register_user(self):
+        response=self.client.post(self.register_url,{},format='')
+        self.assertEqual(response.status_code,302)
