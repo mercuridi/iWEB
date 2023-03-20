9779ff77bbfe41d09552c3a0aca7a7093c72572e
Jude <judegoulding@hotmail.com>
2023-03-20 20:30:32 +0000

    patched with main

--- app/iWEB/iWEB/settings.py
+++ app/iWEB/iWEB/settings.py
@@ -135,3 +135,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_PROFILE_MODULE = "app.UserProfile"
