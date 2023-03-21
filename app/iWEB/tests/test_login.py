34f5d6e066d8a1295f60afc7969ee81bc486a685
Dash <ds702@exeter.ac.uk>
2023-03-21 14:07:15 +0000

    misc and a failing test

--- app/iWEB/tests/test_login.py
+++ app/iWEB/tests/test_login.py
@@ -9,18 +9,18 @@ from django.urls import reverse
 c = Client()
 
 
-class RegisterClass(TestCase):
+class BaseTest(TestCase):
     def setUp(self):
         self.register_url=reverse('register')
         self.user={
-            'username':'username2', 
-            'email':'email@email.com', 
+            'user':'username2', 
+            'email':'anotheremail@email.com', 
             'password1':'password1', 
             'password2':'password1'
         }
         return super().setUp()
 
-class RegisterTest(RegisterClass):
+class RegisterTest(BaseTest):
     def test_can_view_page(self):
         response=self.client.get(self.register_url)
         self.assertEqual(response.status_code, 200) #page is successfully accessed
@@ -29,7 +29,7 @@ class RegisterTest(RegisterClass):
     # places test values into the registration form
     def test_can_register_user(self):
         response=self.client.post(self.register_url,self.user,format='/html')
-        self.assertEqual(response.status_code,302)
+        self.assertEqual(response.status_code,200)
 
 class LoginClass(TestCase):
     def setUp(self):
@@ -37,13 +37,20 @@ class LoginClass(TestCase):
         self.user={
             'username':'username', 
             'email':'email@email.com', 
-            'password1':'password', 
-            'password2':'password'
+            'password1':'password12', 
+            'password2':'password12'
         }
         return super().setUp()
 
-class LoginTest(LoginClass):
+class LoginTest(LoginClass, BaseTest):
     def test_can_view_page(self):
         response=self.client.get(self.login_url)
         self.assertEqual(response.status_code, 200) #page is successfully accessed
         self.assertTemplateUsed(response, 'login.html') #template is successfully used
+    def test_login_success(self):
+        self.client.post(self.register_url,self.user,format='/html')
+        user = User.objects.filter(email=self.user['email']).first()
+        user.is_active=True
+        user.save()
+        response = self.client.post(self.login_url, self.user, format='/html')
+        self.assertEqual(response.status_code, 302)
