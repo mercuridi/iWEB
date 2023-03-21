8902740552f9acc5bbb937cd3f2716625b2be239
Dash <ds702@exeter.ac.uk>
2023-03-21 13:15:57 +0000

    test login and register

--- app/iWEB/tests/test_login.py
+++ app/iWEB/tests/test_login.py
@@ -13,10 +13,10 @@ class RegisterClass(TestCase):
     def setUp(self):
         self.register_url=reverse('register')
         self.user={
-            'username':'username', 
+            'username':'username2', 
             'email':'email@email.com', 
-            'password1':'password', 
-            'password2':'password'
+            'password1':'password1', 
+            'password2':'password1'
         }
         return super().setUp()
 
@@ -26,6 +26,24 @@ class RegisterTest(RegisterClass):
         self.assertEqual(response.status_code, 200) #page is successfully accessed
         self.assertTemplateUsed(response, 'register.html') #template is successfully used
 
+    # places test values into the registration form
     def test_can_register_user(self):
-        response=self.client.post(self.register_url,{},format='')
+        response=self.client.post(self.register_url,self.user,format='/html')
         self.assertEqual(response.status_code,302)
+
+class LoginClass(TestCase):
+    def setUp(self):
+        self.login_url=reverse('login')
+        self.user={
+            'username':'username', 
+            'email':'email@email.com', 
+            'password1':'password', 
+            'password2':'password'
+        }
+        return super().setUp()
+
+class LoginTest(LoginClass):
+    def test_can_view_page(self):
+        response=self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200) #page is successfully accessed
+        self.assertTemplateUsed(response, 'login.html') #template is successfully used
