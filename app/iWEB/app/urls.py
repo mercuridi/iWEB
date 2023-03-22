5f460c3d268a64a576149c0ccc4764d438d52bb7
Mercuridi <kab237@exeter.ac.uk>
2023-03-22 12:49:44 +0000

    remove debug dependencies

--- app/iWEB/app/urls.py
+++ app/iWEB/app/urls.py
@@ -2,11 +2,9 @@
 Module defining the web app's different directories and structures.
 """
 from django.urls import path
-from . import debug, index, login
+from . import index, login
 urlpatterns = [
     path('index', index.main,name='index'),
-    path('home', debug.home, name='home'),
     path('register', login.register_request, name='register'),
     path('', login.login_request, name='login'),
-    path('map', debug.map_view, name='map'),
 ]
