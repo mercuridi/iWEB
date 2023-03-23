855e5038a66275fd5ceac24ce65950231b1d269f
Jude <judegoulding@hotmail.com>
2023-03-23 00:15:17 +0000

    streak replace code

--- app/iWEB/app/index.py
+++ app/iWEB/app/index.py
@@ -26,15 +26,18 @@ def main(request):
         # leaderboard reset button handling
         if data.get("leaderboard_reset") == 0:
             if current_user.is_staff:
+                all_profiles = UserProfile.objects.all()
+                # iterate across users to give them each a new random challenge
+                # (new challenge can be the same as the last one)
+                # also, if they haven't completed their challenge, reset their streak
+                for profile in all_profiles:
+                    UserProfile.objects.filter(id=profile.id).update(current_challenge_id=random.randint(1,3))
+                    UserProfile.objects.filter(challenge_done=False).update(streak=0)
                 # set all users at once to have 0 points this week and an incomplete challenge
                 UserProfile.objects.all().update(
                     points_this_week=0,
                     challenge_done=False,
                     )
-                # iterate across users to give them each a new random challenge
-                # (new challenge can be the same as the last one)
-                for profile in UserProfile.objects.all():
-                    UserProfile.objects.filter(id=profile.id).update(current_challenge_id=random.randint(1,3))
                 
         # streak incrementation and handling upon challenge completion
         location_used = data.get("type_used")
