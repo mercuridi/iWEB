03e18b78e500398f595532f95b1eb1e0727e3040
Jude <judegoulding@hotmail.com>
2023-03-23 01:57:52 +0000

    to expriment wih deleting files

--- app/iWEB/app/urls.py
+++ app/iWEB/app/urls.py
@@ -5,10 +5,8 @@ from django.urls import path
 from . import debug, index, login, help
 urlpatterns = [
     path('index', index.main,name='index'),
-    path('home', debug.home, name='home'),
     path('register', login.register_request, name='register'),
     path('', login.login_request, name='login'),
-    path('map', debug.map_view, name='map'),
     path('addLocation/', index.main, name='addLocation'),
     path('help', help.main, name='help'),
 ]
