24f4cd9c3eafabf906219e8b80f210b20cc9a0a4
Mercuridi <kab237@exeter.ac.uk>
2023-03-23 02:14:33 +0000

    mysql migration

--- app/iWEB/iWEB/settings.py
+++ app/iWEB/iWEB/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'iWEB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'iweb',
+        'USER': 'iweb_dev',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',  
+        'PORT': '3306',
     }
 }
 
